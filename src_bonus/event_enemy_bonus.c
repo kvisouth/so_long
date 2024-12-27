d7f8965e0aea3d170cd25468e00d9e00002849f2
Kevin Visouthivong <kevisout@paul-f4Ar4s2.clusters.42paris.fr>
2024-12-27 15:59:05 +0100

    added threads to increment clock

--- src_bonus/event_enemy_bonus.c
+++ src_bonus/event_enemy_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   event_enemy_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kevso <kevso@student.42.fr>                +#+  +:+       +#+        */
+/*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 13:57:36 by kevso             #+#    #+#             */
-/*   Updated: 2024/12/23 17:29:20 by kevso            ###   ########.fr       */
+/*   Updated: 2024/12/27 15:54:29 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 /* Goblin qui respire */
 void	init_enemy_textures(t_texture *w)
 {
-	w->sprite[0] = "./assets/gob_0.xpm";
-	w->sprite[1] = "./assets/gob_1.xpm";
-	w->sprite[2] = "./assets/gob_2.xpm";
-	w->sprite[3] = "./assets/gob_3.xpm";
-	w->sprite[4] = "./assets/gob_4.xpm";
-	w->sprite[5] = "./assets/gob_5.xpm";
-	w->sprite[6] = "./assets/gob_6.xpm";
-	w->sprite[7] = "./assets/gob_7.xpm";
+	w->sprite[0] = "./assets/bat1.xpm";
+	w->sprite[1] = "./assets/bat2.xpm";
+	w->sprite[2] = "./assets/bat3.xpm";
+	w->sprite[3] = "./assets/bat4.xpm";
+	w->sprite[4] = "./assets/bat5.xpm";
+	w->sprite[5] = "./assets/bat6.xpm";
+	w->sprite[6] = "./assets/bat7.xpm";
+	w->sprite[7] = "./assets/bat8.xpm";
 }
 
 /* "Dessine" la texture goblin.xpm sur la fenetre la ou y'a 'P' sur la map */
@@ -104,6 +104,6 @@ void	put_enemy(t_game *game)
 {
 	anim_enemy(game);
 	get_enemies_pos(game);
-	if (game->x % 300 == 0)
+	if (game->x % 50 == 0)
 		move_enemies(game);
 }
